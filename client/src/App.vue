2781c80daa498470dcfd2d85340b8bdf6de3d4db
Surya-Narayanan0503 <52913089+Surya-Narayanan0503@users.noreply.github.com>
2022-02-27 19:53:48 -0500

    Implemented Vue for to-do app

---
+++ client/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Nav from './components/Nav.vue';
+import Messages from './pages/Messages.vue'; 
+</script>
+
+<template>
+  <Nav />
+  <Messages />
+</template>
+
+
+<style>
+
+</style>
