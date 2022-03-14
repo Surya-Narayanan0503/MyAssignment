922f71773fa583107464b837213cd91bbbb9c621
Surya-Narayanan0503 <52913089+Surya-Narayanan0503@users.noreply.github.com>
2022-03-14 01:04:31 -0400

    Project TODO
    Project TODO

--- Project ToDo App/client/src/models/user.ts
+++ Project ToDo App/client/src/models/user.ts
@@ -12,61 +12,61 @@ export interface User {
 export const list: User[] = [
     {
         id: 1,
-        handle: 'Yash',
-        firstName: 'yash',
-        lastName: 'ghatge',
-        password: 'yash123',
-        email: 'yash@gmail.com',
+        handle: 'Surya',
+        firstName: 'surya',
+        lastName: 'narayanan',
+        password: 'surya',
+        email: 'surya@gmail.com',
         picture: 'https://icon2.cleanpng.com/20180403/rhe/kisspng-computer-icons-avatar-male-super-b-5ac405d4cdcd45.472240621522795988843.jpg',
         userTasks: [
-            {task : 'Need to update vue',dueDate:'04/21/2022', isCompleted: true, assignedBy: "Sakshi"},
-            {task : 'Need to update pinia',dueDate:'05/18/2022', isCompleted: false, assignedBy: "OnePiece"},
-            {task : 'Need to update Rest service',dueDate:'06/11/2022', isCompleted: false, assignedBy: "Harika"},
-            {task : 'Need to update Async',dueDate:'04/17/2022', isCompleted: false, assignedBy: "Onepiece"},
-            {task : 'Need to update express',dueDate:'03/11/2022', isCompleted: true, assignedBy: "Sakshi"}
+            {task : 'Need to update vue',dueDate:'04/21/2022', isCompleted: true, assignedBy: "kokila"},
+            {task : 'Need to update pinia',dueDate:'05/18/2022', isCompleted: false, assignedBy: "ragu"},
+            {task : 'Need to update Rest service',dueDate:'06/11/2022', isCompleted: false, assignedBy: "janani"},
+            {task : 'Need to update Async',dueDate:'04/17/2022', isCompleted: false, assignedBy: "ragu"},
+            {task : 'Need to update express',dueDate:'03/11/2022', isCompleted: true, assignedBy: "kokila"}
         ]
     },
     {
         id: 2,
-        handle: 'Sakshi',
-        firstName: 'sakshi',
-        lastName: 'ghatge',
-        password: 'sakshi123',
-        email: 'lyagami@gmail.com',
+        handle: 'Ragu',
+        firstName: 'ragu',
+        lastName: 'murugaiyan',
+        password: 'ragu',
+        email: 'ragu@gmail.com',
         picture: 'https://www.kindpng.com/picc/m/357-3575561_girl-young-beautiful-people-female-person-woman-user.png',
         userTasks: [
-            {task : 'Need to update angular',dueDate:'03/21/2022', isCompleted: false, assignedBy: "Yash"},
-            {task : 'Need to update react',dueDate:'03/26/2022', isCompleted: false, assignedBy: "Harika"},
-            {task : 'Need to update CSS',dueDate:'04/30/2022', isCompleted: false, assignedBy: "Harika"},
-            {task : 'Need to update Web Service',dueDate:'03/31/2022', isCompleted: false, assignedBy: "Yash"}
+            {task : 'Need to update angular',dueDate:'03/21/2022', isCompleted: false, assignedBy: "Surya"},
+            {task : 'Need to update react',dueDate:'03/26/2022', isCompleted: false, assignedBy: "Janani"},
+            {task : 'Need to update CSS',dueDate:'04/30/2022', isCompleted: false, assignedBy: "Janani"},
+            {task : 'Need to update Web Service',dueDate:'03/31/2022', isCompleted: false, assignedBy: "Surya"}
         ]
         //
     },
     {
         id: 3,
-        handle: 'Harika',
-        firstName: 'Hari',
-        lastName: 'Rex',
-        password: 'hari123',
-        email: 'l@gmail.com',
+        handle: 'Janani',
+        firstName: 'janu',
+        lastName: 'ragupathy',
+        password: 'janani',
+        email: 'janani@gmail.com',
         picture: 'https://www.pngitem.com/pimgs/m/461-4610786_beautiful-girl-cartoon-png-clipart-png-download-beautiful.png',
         userTasks: [
-            {task : 'Need to update jQuery',dueDate:'04/05/2022', isCompleted: false, assignedBy: "Sakshi"},
-            {task : 'Need to update Services',dueDate:'07/26/2022', isCompleted: false, assignedBy: "Yash"},
-            {task : 'Need to update Python',dueDate:'03/22/2022', isCompleted: false, assignedBy: "Yash"},
-            {task : 'Need to update ajax',dueDate:'03/25/2022', isCompleted: false, assignedBy: "Yash"}
+            {task : 'Need to update jQuery',dueDate:'04/05/2022', isCompleted: false, assignedBy: "Kokila"},
+            {task : 'Need to update Services',dueDate:'07/26/2022', isCompleted: false, assignedBy: "Surya"},
+            {task : 'Need to update Python',dueDate:'03/22/2022', isCompleted: false, assignedBy: "Surya"},
+            {task : 'Need to update ajax',dueDate:'03/25/2022', isCompleted: false, assignedBy: "Surya"}
         ]
     },
     {
         id: 4,
-        handle: 'Niha',
-        firstName: 'Niha',
-        lastName: 'Akta',
-        password: 'niha123',
-        email: 'Niha@gmail.com',
+        handle: 'Kokila',
+        firstName: 'koki',
+        lastName: 'ragupathy',
+        password: 'kokila',
+        email: 'koki@gmail.com',
         picture: 'https://images.cdn1.stockunlimited.net/clipart/customer-service-executive_1562515.jpg',
         userTasks: [
-            {task : 'Need to update Vue',dueDate:'20/05/2022', isCompleted: false, assignedBy: "Sakshi"},
+            {task : 'Need to update Vue',dueDate:'20/05/2022', isCompleted: false, assignedBy: "kokila"},
             {task : 'Need to update ajax',dueDate:'03/25/2022', isCompleted: false, assignedBy: "Mishat"}
         ]
     },
@@ -79,8 +79,8 @@ export const list: User[] = [
         email: 'mishat@gmail.com',
         picture: 'https://images.cdn3.stockunlimited.net/clipart/call-center-executive_1617805.jpg',
         userTasks: [
-            {task : 'Need to update Vue',dueDate:'04/09/2022', isCompleted: false, assignedBy: "Sakshi"},
-            {task : 'Need to update ajax',dueDate:'06/25/2022', isCompleted: false, assignedBy: "Niha"}
+            {task : 'Need to update Vue',dueDate:'04/09/2022', isCompleted: false, assignedBy: "Janani"},
+            {task : 'Need to update ajax',dueDate:'06/25/2022', isCompleted: false, assignedBy: "Kokila"}
         ]
     }
 ]
