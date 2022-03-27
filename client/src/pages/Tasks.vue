42f78dd643c9441c7daaf67fff3715348d3a9799
Surya-Narayanan0503 <surya.ragu0503@gmail.com>
2022-03-27 10:02:16 +0500

    taskspage

---
+++ client/src/pages/Tasks.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const tabs = ['Assigned', 'Created', 'All'];
+const currentTab = ref(tabs[0]);
+
+const tabClass = (tab: string) => tab === currentTab.value ? 'tab active' : 'tab';
+
+</script>
+
+<template>
+	<h1>T O D O</h1>
+	<div class="card tabs">
+		<div :class="tabClass(tab)" v-for="tab in tabs" @click="() => (currentTab = tab)">{{ tab }}</div>
+	</div>
+	<div class="card add">Add</div>
+</template>
+
+<style scoped lang="scss">
+.add {
+	position: absolute;
+	bottom: 0;
+	left: 20%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100px;
+	height: 40px;
+	font-weight: 600;
+	color: #4285f4;
+	cursor: pointer;
+}
+
+
+.tabs {
+	position: absolute;
+	bottom: 0;
+	left: 50%;
+	transform: translateX(-50%);
+	display: flex;
+	flex-direction: row;
+	justify-content: space-evenly;
+	align-items: center;
+	width: 500px;
+	height: 40px;
+	margin: 0;
+
+	.tab {
+		color: #abc9f8;
+		font-weight: 600;
+		cursor: pointer;
+	}
+
+	.active {
+		color: #4285f4;
+	}
+}
+
+h1 {
+	position: absolute;
+	color: #4285f4;
+	font-size: 36px;
+	font-weight: 800;
+	top: 100px;
+	left: 50%;
+	transform: translateX(-50%);
+}
+</style>
