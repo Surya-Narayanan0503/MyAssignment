26a1afe8a86be1b9168fd3426fa39aa16f4c4bd9
Surya-Narayanan0503 <52913089+Surya-Narayanan0503@users.noreply.github.com>
2022-05-09 11:35:24 -0400

    Removed Merge Conflicts

--- client/src/models/session.ts
+++ client/src/models/session.ts
@@ -15,19 +15,14 @@ export interface ISession {
 export const session: ISession = reactive<ISession>({
 	isLoggedIn: 'false',
 	token: null,
-
 	username: null,
 	email: null
-
-	username: null
-
 });
 
 export const endSession = (cookies: Cookies) => {
 	session.isLoggedIn = 'false';
 	session.token = null;
 	session.username = null;
-
 	session.email = null;
 	cookies.set('isLoggedIn', 'false');
 	cookies.set('token', '');
@@ -37,38 +32,21 @@ export const endSession = (cookies: Cookies) => {
 
 export const setSession = (
 	{ token, username, isLoggedIn, email }: ISession,
-
-	cookies.set('isLoggedIn', 'false');
-	cookies.set('token', '');
-	cookies.set('username', '');
-}
-
-export const setSession = (
-	{ token, username, isLoggedIn }: ISession,
-
 	cookies: Cookies
 ) => {
 	session.isLoggedIn = isLoggedIn;
 	session.token = token;
 	session.username = username;
-
 	session.email = email;
 	cookies.set('isLoggedIn', 'true');
 	cookies.set('username', username ?? '');
 	cookies.set('token', token ?? '');
 	cookies.set('email', email ?? '');
-
-	cookies.set('isLoggedIn', 'true');
-	cookies.set('username', username ?? '');
-	cookies.set('token', token ?? '');
-
 };
 
 export const getSession = (cookies: Cookies) => {
 	session.isLoggedIn = cookies.get('isLoggedIn') == 'true' ? 'true' : 'false';
 	session.token = cookies.get('token') || null;
 	session.username = cookies.get('username') || null;
-
 	session.email = cookies.get('email') || null;
-
 };
