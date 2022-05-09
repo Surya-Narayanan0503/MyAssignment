3d4727cb65e9cee979ef43016a103b2bad34817d
Surya-Narayanan0503 <52913089+Surya-Narayanan0503@users.noreply.github.com>
2022-05-09 11:25:54 -0400

    Removed Merge Conflicts

--- client/src/models/session.ts
+++ client/src/models/session.ts
@@ -15,19 +15,19 @@ export interface ISession {
 export const session: ISession = reactive<ISession>({
 	isLoggedIn: 'false',
 	token: null,
-<<<<<<< HEAD
+
 	username: null,
 	email: null
-=======
+
 	username: null
->>>>>>> ccdfd4e8ba06c1c2f5f0019def4e0e2ea947bad6
+
 });
 
 export const endSession = (cookies: Cookies) => {
 	session.isLoggedIn = 'false';
 	session.token = null;
 	session.username = null;
-<<<<<<< HEAD
+
 	session.email = null;
 	cookies.set('isLoggedIn', 'false');
 	cookies.set('token', '');
@@ -37,7 +37,7 @@ export const endSession = (cookies: Cookies) => {
 
 export const setSession = (
 	{ token, username, isLoggedIn, email }: ISession,
-=======
+
 	cookies.set('isLoggedIn', 'false');
 	cookies.set('token', '');
 	cookies.set('username', '');
@@ -45,31 +45,30 @@ export const setSession = (
 
 export const setSession = (
 	{ token, username, isLoggedIn }: ISession,
->>>>>>> ccdfd4e8ba06c1c2f5f0019def4e0e2ea947bad6
+
 	cookies: Cookies
 ) => {
 	session.isLoggedIn = isLoggedIn;
 	session.token = token;
 	session.username = username;
-<<<<<<< HEAD
+
 	session.email = email;
 	cookies.set('isLoggedIn', 'true');
 	cookies.set('username', username ?? '');
 	cookies.set('token', token ?? '');
 	cookies.set('email', email ?? '');
-=======
+
 	cookies.set('isLoggedIn', 'true');
 	cookies.set('username', username ?? '');
 	cookies.set('token', token ?? '');
->>>>>>> ccdfd4e8ba06c1c2f5f0019def4e0e2ea947bad6
+
 };
 
 export const getSession = (cookies: Cookies) => {
 	session.isLoggedIn = cookies.get('isLoggedIn') == 'true' ? 'true' : 'false';
 	session.token = cookies.get('token') || null;
 	session.username = cookies.get('username') || null;
-<<<<<<< HEAD
+
 	session.email = cookies.get('email') || null;
-=======
->>>>>>> ccdfd4e8ba06c1c2f5f0019def4e0e2ea947bad6
+
 };
