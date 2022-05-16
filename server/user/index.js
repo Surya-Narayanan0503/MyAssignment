f1f21feed7089bd21f12d7621853cbd7b43a8956
Surya-Narayanan0503 <52913089+Surya-Narayanan0503@users.noreply.github.com>
2022-05-16 16:49:50 -0400

    modified folder structure

--- server/user/index.js
+++ server/user/index.js
@@ -2,28 +2,17 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const express = require('express');
 
-<<<<<<< HEAD
 const authMiddleware = require('../auth/middleware');
-=======
-const authMiddleware  = require('../auth/middleware');
->>>>>>> ccdfd4e8ba06c1c2f5f0019def4e0e2ea947bad6
 const router = express.Router();
 
 const userSchema = new mongoose.Schema({
     userName: String,
     password: String,
-<<<<<<< HEAD
     email: String
 });
 
 userSchema.methods.generateAuthToken = function (userId, userName, email) {
     const token = jwt.sign({ sub: userId, userName, email }, "someprivatekey");
-=======
-});
-
-userSchema.methods.generateAuthToken = function (userId, userName) {
-    const token = jwt.sign({ sub: userId, userName }, "someprivatekey");
->>>>>>> ccdfd4e8ba06c1c2f5f0019def4e0e2ea947bad6
     return token;
 }
 
@@ -54,10 +43,7 @@ router.post('/', async (req, res) => {
         let user = new User({
             userName: req.body.userName,
             password: req.body.password,
-<<<<<<< HEAD
             email: req.body.email
-=======
->>>>>>> ccdfd4e8ba06c1c2f5f0019def4e0e2ea947bad6
         });
         let createdUser = await user.save();
         res.status(201).send(createdUser);
@@ -68,12 +54,7 @@ router.post('/', async (req, res) => {
 });
 
 module.exports = {
-<<<<<<< HEAD
     User: User,
     userSchema: userSchema,
-=======
-    User : User,
-    userSchema : userSchema,
->>>>>>> ccdfd4e8ba06c1c2f5f0019def4e0e2ea947bad6
     userRouter: router
 };  
